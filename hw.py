34abe62c0e52882d69df3a0e5d64046ede8b907e
Boba Mox <bobamox@mail.ru>
2024-07-01 14:50:41 +0300

    1 modified hw.py

--- hw.py
+++ hw.py
@@ -3,27 +3,21 @@ count_lines_1 = 0
 count_lines_2 = 0
 count_lines_3 = 0
 
-with open('4.txt', 'w') as f_4:
-    with open('files/1.txt') as f_1:
-        lines = f_1.readlines()
-        for line in lines:
-            count_lines_1 += 1
-            dict_txt['1_txt']=count_lines_1, line
-    with open('files/2.txt') as f_2:
-        lines = f_2.readlines()
-        for line in lines:
-            count_lines_2 += 1
-            dict_txt['2_txt']=count_lines_2, line
-    with open('files/3.txt') as f_3:
-        lines = f_3.readlines()
-        for line in lines:
-            count_lines_3 += 1
-            dict_txt['3_txt']=count_lines_3, line
+
+with open('files/1.txt') as f_1:
+    lines = f_1.readlines()
+    for line in lines:
+        count_lines_1 += 1
+        dict_txt['1_txt'] = count_lines_1, line
+with open('files/2.txt') as f_2:
+    lines = f_2.readlines()
+    for line in lines:
+        count_lines_2 += 1
+        dict_txt['2_txt'] = count_lines_2, line
+with open('files/3.txt') as f_3:
+    lines = f_3.readlines()
+    for line in lines:
+        count_lines_3 += 1
+        dict_txt['3_txt'] = count_lines_3, line
 
 print(dict_txt)
-
-
-
-
-
-
