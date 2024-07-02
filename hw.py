7d87c556b4425cee7743bb11a7767eaf361806be
Boba Mox <bobamox@mail.ru>
2024-07-02 07:58:11 +0300

    Change hw.py and added info in 4.txt

--- hw.py
+++ hw.py
@@ -1,23 +1,32 @@
+from pprint import pprint
 dict_txt = {}
 count_lines_1 = 0
 count_lines_2 = 0
 count_lines_3 = 0
-
+text_1 = ''
+text_2 = ''
+text_3 = ''
 
 with open('files/1.txt') as f_1:
     lines = f_1.readlines()
     for line in lines:
         count_lines_1 += 1
-        dict_txt['1_txt'] = count_lines_1, line
+        text_1 += line.strip('\n')
+        dict_txt['1_txt'] = count_lines_1, text_1
 with open('files/2.txt') as f_2:
     lines = f_2.readlines()
     for line in lines:
         count_lines_2 += 1
-        dict_txt['2_txt'] = count_lines_2, line
+        text_2 += line.strip('\n')
+        dict_txt['2_txt'] = count_lines_2, text_2
 with open('files/3.txt') as f_3:
     lines = f_3.readlines()
     for line in lines:
         count_lines_3 += 1
-        dict_txt['3_txt'] = count_lines_3, line
+        text_3 += line.strip('\n')
+        dict_txt['3_txt'] = count_lines_3, text_3
 
-print(dict_txt)
+pprint(dict_txt)
+
+with open('4.txt', 'w') as f_4:
+    f_4.write(f'{dict_txt}\n')
